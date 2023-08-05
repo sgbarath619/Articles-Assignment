5c1bac2e56da4772e2f20f7b2afd83c013c10f05
sgbarath619 <sgbarath619@gmail.com>
2023-08-05 13:38:20 +0530

    final commit

--- config/initializers/razorpay.rb
+++
@@ -1,5 +0,0 @@
-require "razorpay"
-
-key_id = Rails.application.credentials.dig(:razorpay, :key_id)
-secret_key = Rails.application.credentials.dig(:razorpay, :secret_key)
-Razorpay.setup(key_id, secret_key)
